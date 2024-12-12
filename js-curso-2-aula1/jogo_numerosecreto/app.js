e3739a8d641375f86272f17d6115c62568e37d6e
Thiago Zibetti <thiago.zibetti95@gmail.com>
2024-12-12 14:15:47 -0300

    teste Thiago

--- js-curso-2-aula1/jogo_numerosecreto/app.js
+++ js-curso-2-aula1/jogo_numerosecreto/app.js
@@ -19,16 +19,15 @@ exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
     if (chute == numeroSecreto) {
         exibirTextoNaTela ('h1','Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela ('p',mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     }
     else {
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let maiorMenor = chute > numeroSecreto ? 'menor':'maior'
         let msgMaiorMenor = `O número é ${maiorMenor} que ${chute}! Você usou ${tentativas} ${palavraTentativa}`;
     
