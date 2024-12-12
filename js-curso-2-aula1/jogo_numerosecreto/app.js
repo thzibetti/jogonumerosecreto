529449b9b2ea4818c0d2fd835dd3b7cab12ef7e0
Thiago Zibetti <thiago.zibetti95@gmail.com>
2024-12-12 14:48:00 -0300

    Mudança da quantidade de jogo e correção

--- js-curso-2-aula1/jogo_numerosecreto/app.js
+++ js-curso-2-aula1/jogo_numerosecreto/app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo: Número secreto do Thiago');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 console.log(numeroSecreto);
